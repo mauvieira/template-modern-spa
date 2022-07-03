45e2fd0896be8da9251fc82c3860bf838ae1037f
mauvieira <contato@vieiramauricio.com>
2022-07-02 21:26:53 -0300

    refactor: moved lazy import to a separate function

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import("./mocks/browser").then(({ worker }) => {
-  worker.start({ onUnhandledRequest: "bypass" });
+const prepare = async (): Promise<void> => {
+  if (import.meta.env.DEV) {
+    const { worker } = await import("./mocks/browser");
+    worker.start();
+  }
+};
 
+prepare().then(() => {
   ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
       <App />
