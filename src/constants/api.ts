8b1f82c25b3607a436a06df97b84e0e883e93326
mauvieira <contato@vieiramauricio.com>
2022-07-02 22:01:54 -0300

    feature: add env variable

---
+++ src/constants/api.ts
@@ -0,0 +1,1 @@
+export const apiBaseUrl = import.meta.env.VITE_API_HOST;
--- src/example/components/User/User.tsx
+++ src/example/components/User/User.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
+import { apiBaseUrl } from "../../../constants/api";
 
 export const User = () => {
   const [user, setUser] = useState();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:3000/user")
+    fetch(apiBaseUrl + "user")
       .then((response) => response.json())
       .then((data) => setUser(data))
       .finally(() => setLoading(false));
@@ -14,6 +15,6 @@ export const User = () => {
   if(loading) {
     return <span>loading...</span>
   }
-  
+
   return <div>{JSON.stringify(user, null, 4)}</div>;
 };
